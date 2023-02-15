8a963299bf51f0918cffe3af114a7a6d162355ea
SK SOHEL RAHAMAN <117460184+sohel-777@users.noreply.github.com>
2023-02-15 13:20:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 // your code here
+function formSubmit(event) {
+	
+	event.preventDefault();
+	
+	var h3=document.querySelector("#url");
+	var h3Value =  "https://localhost:8080/"
+	var name=document.querySelector("#name").value;
+	var year=document.querySelector("#year").value;
+	
+	
+	if (name && year) {
+	    h3Value += "?name=" + name + "&year=" + year;
+	  } else if (name && !year) {
+	   h3Value += "?name=" + name;
+	  } else if (!name && year) {
+	   h3Value += "?year=" + year;
+	  }
+	h3.innerText=h3Value;
+}
+document.querySelector("form").addEventListener('submit', formSubmit);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
